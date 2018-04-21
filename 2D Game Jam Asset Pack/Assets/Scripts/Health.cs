4f0cffeb5eb6d11bff3576b7892dc5ab42b3c499
Sami Zirak <samizirakdev@gmail.com>
2018-04-21 15:43:57 +0000

    Health System Script
    Implemented Health System. Linked to previously pushed UI.

---
+++ 2D Game Jam Asset Pack/Assets/Scripts/Health.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    public int numOfHearts;
+
+    public Image [] heartContainers;
+    public Sprite fullHeartSprite;
+    public Sprite emptyHeartSprite;
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < heartContainers.Length; i++)
+        {
+
+            if(i < health)
+            {
+                heartContainers[i].sprite = fullHeartSprite;
+            }
+            else
+            {
+                heartContainers[i].sprite = emptyHeartSprite;
+            }
+
+            if(i < numOfHearts)
+            {
+                heartContainers[i].enabled = true;
+            }
+            else
+            {
+                heartContainers[i].enabled = false;
+            }
+
+
+        }
+
+	}
+}

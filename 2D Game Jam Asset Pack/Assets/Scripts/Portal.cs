56a311e5820eaaaed99272669771ceed2b9021ff
Sami Zirak <samizirakdev@gmail.com>
2018-04-24 23:07:30 +0000

    Added In Portals
    Added Portal Script aswell as Prototyping Portal Prefab with Animations. PSD & Sprites added as well.

---
+++ 2D Game Jam Asset Pack/Assets/Scripts/Portal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public GameObject player;
+
+    public GameObject portal;
+
+    public string playerTag;
+
+    public float teleportDelay;
+
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == playerTag & Input.GetButtonDown("Interact"))
+        {
+            StartCoroutine(Teleport(teleportDelay));
+        }
+    }
+
+    IEnumerator Teleport(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        player.transform.position = new Vector2(portal.transform.position.x , portal.transform.position.y);
+    }
+
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}

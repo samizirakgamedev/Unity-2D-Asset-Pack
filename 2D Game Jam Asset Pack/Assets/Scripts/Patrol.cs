2eb3e8591bcf58f08b5a476e0922fea4b650dade
Sami Zirak <samizirakdev@gmail.com>
2018-04-25 18:13:29 +0000

    Added In Patrol Script
    Created Patrol Script for enemies and imported Spiked_Enemy sprites for prototyping. Prefab of Spiked Enemy created. Asset pack files organised into folders.

---
+++ 2D Game Jam Asset Pack/Assets/Scripts/Patrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+    public float patrolSpeed;
+    public float groundRaycastLength;
+
+    public Transform groundPoint;
+
+    private bool movingLeft = true;
+
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        transform.Translate(Vector2.left * patrolSpeed * Time.deltaTime);
+
+        RaycastHit2D groundRaycast = Physics2D.Raycast(groundPoint.position, Vector2.down, groundRaycastLength);
+
+        if(groundRaycast.collider == false)
+        {
+            if(movingLeft == true)
+            {
+                transform.eulerAngles = new Vector2(0, -180);
+                movingLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector2(0, 0);
+                movingLeft = true;
+            }
+        }
+	}
+}

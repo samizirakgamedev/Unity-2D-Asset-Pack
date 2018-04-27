1c1919a8dce5abd206ce6609409465df960bc52b
Sami Zirak <samizirakdev@gmail.com>
2018-04-27 15:02:14 +0000

    Scrolling Background & Camera Bounds
    Setup a Scrolling Background in Test Scene using ScrollBackground script that I created. Added bounds to camera to prevent seeing unwanted areas of the screen. Prefab of background added to prefab folder.

---
+++ 2D Game Jam Asset Pack/Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour {
+
+    public Transform backgroundOne;
+    public Transform backgroundTwo;
+
+    public Camera gameCam;
+
+    public float currentWidth;
+    public float bgWidthGain;
+
+    private bool whichBackground = true;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if(currentWidth < gameCam.transform.position.x)
+        {
+            if(whichBackground)
+            {
+                backgroundOne.localPosition = new Vector2(backgroundOne.localPosition.x + bgWidthGain, 0);
+            }
+            else
+            {
+                backgroundTwo.localPosition = new Vector2(backgroundTwo.localPosition.x + bgWidthGain, 0);
+            }
+
+            currentWidth += 15;
+
+            whichBackground = !whichBackground;
+        }
+
+        if(currentWidth > gameCam.transform.position.x + 15)
+        {
+            if (whichBackground)
+            {
+                backgroundTwo.localPosition = new Vector2(backgroundTwo.localPosition.x - bgWidthGain, 0);
+            }
+            else
+            {
+                backgroundOne.localPosition = new Vector2(backgroundOne.localPosition.x - bgWidthGain, 0);
+            }
+
+            currentWidth -= 15;
+
+            whichBackground = !whichBackground;
+        }
+	}
+}

3d63ec379fd5116964b8c5f29dceba67a125b109
Sami Zirak <samizirakdev@gmail.com>
2018-05-05 16:24:47 +0000

    Enemy Finite State AI Prep
    Prepaired to code enemy finite state machine AI by setting up anitations and animation transitions.

---
+++ 2D Game Jam Asset Pack/Assets/Scripts/SkyFollow2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyFollow2D : MonoBehaviour {
+
+    public Camera gameCam;
+
+    public Vector3 objOffset;
+
+    private GameObject skyObj;
+
+    void Start ()
+    {
+        skyObj = GetComponent<GameObject>();
+	}
+	
+
+	void Update ()
+    {
+        Vector3 objPosition = objOffset;
+
+        objPosition.x += gameCam.transform.position.x;
+
+        //gameCam.transform.position = objPosition;
+	}
+}
